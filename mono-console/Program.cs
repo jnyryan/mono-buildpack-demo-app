fc2ff118e013a6fbec1aad244ce9801c2fa746ba
jnyryan <jnyryan@glgroup.com>
2015-04-27 14:48:12 +0100

    Added the most basic console ever

---
+++ mono-console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BRRP_SYSTEM_MONO. This should be passed through to the logger");
+        }
+    }
+}
