871f07adb6f0113f7ad2d6effbfa56d1b8f73c9d
jnyryan <jnyryan@gmail.com>
2015-06-05 13:16:57 +0100

    upadted env

--- mono-console/Program.cs
+++ mono-console/Program.cs
@@ -11,7 +11,9 @@ namespace MonoConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Mono-Console-App. This should be passed through to the logger");
-            Console.WriteLine("APP Name" + ConfigurationManager.AppSettings["name"]);
+            Console.WriteLine("APP Name: " + ConfigurationManager.AppSettings["app_name"]);
+            Console.WriteLine("APP Name: " + Environment.GetEnvironmentVariable("APP_NAME"));
+            Console.WriteLine("APP Version: " + ConfigurationManager.AppSettings["app_version"]);
         }
     }
 }
--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 echo "Attempting to run mono-console-app"
 logger "Attempting to run mono-console-app"
 pwd
+
+echo $APP_NAME
 mono ./build/mono-console.exe
