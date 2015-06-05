50cf0884c7426fe685125c5d1a64dbba9c1af54b
jnyryan <jnyryan@gmail.com>
2015-06-05 12:58:10 +0100

    upadted env

--- mono-console/Program.cs
+++ mono-console/Program.cs
@@ -11,7 +11,7 @@ namespace MonoConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Mono-Console-App. This should be passed through to the logger");
-            Console.WriteLine("APP Name" + ConfigurationManager.AppSettings["%APP_NAME%"]);
+            Console.WriteLine("APP Name" + ConfigurationManager.AppSettings["name"]);
         }
     }
 }
