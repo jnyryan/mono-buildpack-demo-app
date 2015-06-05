46fb8a642660ea69df4f42a20b5c09eaa82aee14
jnyryan <jnyryan@gmail.com>
2015-06-05 14:35:42 +0100

    added smple http server

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 echo "Attempting to run mono-console-app"
-logger "Attempting to run mono-console-app"
-pwd
 
 echo $APP_NAME
 mono ./build/mono-console.exe
+
+python -m SimpleHTTPServer 3000
